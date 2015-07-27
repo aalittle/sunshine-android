49b5f372b9bfbd6a25cc6bf2cdbbcc1a5e479411
Andrew Little <alittle@salesforce.com>
2015-07-26 21:05:28 -0300

    Completed Lesson 1

--- app/src/main/java/com/example/aalittle/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/aalittle/sunshine/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +22,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Today - Sunny - 88/63");
+        list.add("Tomorrow - Rainy - 90/80");
+        list.add("Tuesday - Cloudy - 60/40");
+        list.add("Wednesday - Sunny - 70/60");
+        list.add("Thursday - Sunny - 100/90");
+        list.add("Friday - Cloudy - 60/40");
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, list);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
