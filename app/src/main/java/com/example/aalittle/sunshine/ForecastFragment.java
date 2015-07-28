987b112375e4daceeb2fc8000fd98eef7242e6ae
Andrew Little <alittle@salesforce.com>
2015-07-27 22:35:35 -0300

    Lesson 3 - update list of forecasts based on the saved location (postal code)

--- app/src/main/java/com/example/aalittle/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/aalittle/sunshine/ForecastFragment.java
@@ -3,9 +3,11 @@ package com.example.aalittle.sunshine;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -25,6 +27,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.prefs.PreferenceChangeEvent;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -58,12 +61,6 @@ public class ForecastFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         ArrayList<String> list = new ArrayList<String>();
-        list.add("Today - Sunny - 88/63");
-        list.add("Tomorrow - Rainy - 90/80");
-        list.add("Tuesday - Cloudy - 60/40");
-        list.add("Wednesday - Sunny - 70/60");
-        list.add("Thursday - Sunny - 100/90");
-        list.add("Friday - Cloudy - 60/40");
 
         mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, list);
 
@@ -85,11 +82,27 @@ public class ForecastFragment extends Fragment {
             }
         });
 
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
-        weatherTask.execute("b3h1m2");
-
         return rootView;
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        weatherTask.execute(getLocationPreference());
+    }
+
+    private String getLocationPreference() {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String locationPref = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        return locationPref;
     }
 
     @Override
@@ -104,8 +117,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if(id == R.id.action_refresh) {
 
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("b3h1m2");
+            updateWeather();
 
             return true;
         }
